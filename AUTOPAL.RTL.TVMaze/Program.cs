53c75a4340498fbba67bf818b12a1faa33eeb5b9
Aytac Utku TOPAL <aytacutku@hotmail.com>
2018-11-27 21:47:46 +0100

    Add project files.

---
+++ AUTOPAL.RTL.TVMaze/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AUTOPAL.RTL.TVMaze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
